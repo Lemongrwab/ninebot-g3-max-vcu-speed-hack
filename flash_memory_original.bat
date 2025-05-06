c70b0e42d42b21f49c10d8269fba5a1195f6b5c7
Sharki4 <sharkcffg@gmail.com>
2025-05-06 23:41:16 +0300

    flash original

---
+++ flash_memory_original.bat
@@ -0,0 +1,18 @@
+@echo off
+IF EXIST MEMORY_G3.bin (
+	openocd -s oocd/scripts -f 
+	oocd/scripts/interface/stlink.cfg
+	 -f oocd/scripts/target/at32.cfg 
+	 -c "init" 
+	 -c "reset halt" 
+	 -c "flash probe 0"
+	 -c "stm32f1x unlock 0" 
+	 -c "reset halt" 
+	 -c "stm32f1x mass_erase 0" 
+	 -c "flash write_bank 0 MEMORY_G3.bin"
+	  -c "reset run" 
+	  -c "exit"
+) ELSE (
+	echo MEMORY_G3.bin missing.
+)
+pause
